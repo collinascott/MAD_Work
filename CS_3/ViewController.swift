36f324195b209c7e0266ec53c884d40fc7526ae9
Collin <collinascott@gmail.com>
2018-02-21 21:04:21 -0500

    Added ignore and attribute
     On branch master
     Changes to be committed:
    	new file:   .gitattributes
    	new file:   .gitignore
    	modified:   CS_3/Base.lproj/Main.storyboard
    	modified:   CS_3/ViewController.swift

--- CS_3/ViewController.swift
+++ CS_3/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var tipLabel: UILabel!
 
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var totalLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +25,19 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func calculateTip(_ sender: Any) {
+        let bill = Double(billField.text!) ?? 0
+        let tipPercentages = [0.04, 0.12, 0.19, 0.14]
+
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    @IBAction func onTap(_ sender: Any) {
+       view.endEditing(true)
+    }
 }
 
